f5cc49b26a00e1cae0fa68442e3bf6b81dc3d4d2
Evelin <evelindacanovas@gmail.com>
2021-07-24 11:19:07 +0200

    Revert "tc1 first add"
    This reverts commit 19e98b5f

--- src/test/java/TC1.java
+++ src/test/java/TC1.java
@@ -1,7 +1,5 @@
 public class TC1 {
     public static void main(String[] args) {
         System.out.println("firt addition");
-        //I deleted here second addition
-        System.out.println("third");
     }
 }
